f24b4d58d119844695ec1baf216a05a11445df9a
Léo Millon <millon.leo@gmail.com>
2014-03-10 00:06:28 +0100

    Date formatter for mustache templates

--- index.js
+++ index.js
@@ -9,9 +9,27 @@ app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	var dataValue = 1;
+	var dataValue = Date.now();
 	debug('Index page requested with data = ' + dataValue);
-	res.render('index', { data : dataValue });
+	res.render('index', { 
+			data : dataValue,
+			util: {
+				date: {
+					date: function () {
+						return function(text, render) {
+							var textValue = render(text),
+								timestamp = parseInt(textValue),
+								date = new Date(timestamp);
+							debug("text = " + text);
+							debug("textValue = " + textValue);
+							debug("timestamp = " + timestamp);
+							debug("date = " + date);
+							return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+						}
+					}
+				}
+			}
+		});
 });
 
 var server = app.listen(8080, function() {
