1fb6cfeca62fb8ecf620fba1650fe5f8ec46bf0e
Léo Millon <millon.leo@gmail.com>
2014-03-09 17:10:40 +0100

    Use of 'consolidate' as template manager abstraction for mustache

--- index.js
+++ index.js
@@ -1,37 +1,17 @@
 var express = require('express'),
-	path = require('path'),
-	fs = require('fs'),
-	mustache = require('mustache');
+	cons = require('consolidate'),
+	app = express();
 
-var app = express(),
-	viewsDir = path.join(__dirname, 'views'),
-	serverPort = 8080;
+var serverPort = 8080;
 
-function getContents(fileName, extension) {
-	return fs.readFileSync(path.join(viewsDir, fileName + '.' + extension), 'utf8');
-}
+app.engine('html', cons.mustache);
 
-function getFile(fileName, extension) {
-	var view = getContents(fileName, extension);
-	if (!view) {
-		throw new Error('Cannot find view for name : "' + fileName + '.' + extension + '"');
-	}
-	return view;
-}
-
-function getView(viewName) {
-	return getFile(viewName, 'html');
-}
-
-function renderView(viewName, data) {
-	var view = getView(viewName);
-	return mustache.render(view, data);
-}
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	res.setHeader('Content-Type', 'text/html');
-	res.end(renderView('index', { testNum : 1 }));
+	res.render('index', { testNum : 1 });
 });
 
 app.listen(serverPort);
-console.log('Server started (port : ' + serverPort + ')');
+console.log('Server listening on port ' + serverPort);
