78b3391a77791d684a5901203afeab349c6b452b
Léo Millon <millon.leo@gmail.com>
2014-03-10 23:35:00 +0100

    Lambdas are accessible from every view

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 var express = require('express'),
-	cons = require('consolidate'),
-	debug = require('debug')('3to:dev');
-	app = express();
+    cons = require('consolidate'),
+    debug = require('debug')('3to:dev');
+    app = express();
 
-app.engine('html', cons.mustache);
+app.configure(function() {
+    app.set('view engine', 'html');
+    app.set('views', __dirname + '/views');
+    app.engine('html', cons.mustache);
 
-app.set('view engine', 'html');
-app.set('views', __dirname + '/views');
+    app.locals({
+        // mustache lambdas
+        util : {
+            date: {
+                date: function () {
+                    return function(text, render) {
+                        var textValue = render(text),
+                            timestamp = parseInt(textValue),
+                            date = new Date(timestamp);
+                        debug("text = " + text);
+                        debug("textValue = " + textValue);
+                        debug("timestamp = " + timestamp);
+                        debug("date = " + date);
+                        return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
+                    }
+                }
+            }
+        }
+    });
+});
 
 app.get('/', function(req, res) {
-	var dataValue = Date.now();
-	debug('Index page requested with data = ' + dataValue);
-	res.render('index', { 
-			data : dataValue,
-			util: {
-				date: {
-					date: function () {
-						return function(text, render) {
-							var textValue = render(text),
-								timestamp = parseInt(textValue),
-								date = new Date(timestamp);
-							debug("text = " + text);
-							debug("textValue = " + textValue);
-							debug("timestamp = " + timestamp);
-							debug("date = " + date);
-							return date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear();
-						}
-					}
-				}
-			}
-		});
+    var dataValue = Date.now();
+    debug('Index page requested with data = ' + dataValue);
+    res.render('index', {
+        data : dataValue
+    });
 });
 
 var server = app.listen(8080, function() {
-	debug('Listening on port %d', server.address().port);
+    debug('Listening on port %d', server.address().port);
 });
